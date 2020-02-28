52d1dc3308eb30765a153ab8219d4aa265eec861
Takuya OHASHI <tky.ohashi@gmail.com>
2020-02-28 14:38:09 +0900

    Update 007

--- 007_reverse_integer/reverse_integer.c
+++ 007_reverse_integer/reverse_integer.c
@@ -5,13 +5,12 @@ int reverse(int x){
 #define INT_MIN (-2147483648)
     int r;
     while(1) {
-        r += x % 10;
+        int d = x % 10;
+        r += d;
         x /= 10;
         if(!x) break;
-        if(r > INT_MAX/10 || r < INT_MIN/10) {
-            r = 0;
-            break;
-        }
+        if(r > INT_MAX/10 || (r == INT_MAX / 10 && d == 7)) return 0;
+        if(r < INT_MIN/10 || (r == INT_MIN / 10 && d == 8)) return 0;
         r *= 10;
     }
     return r;
