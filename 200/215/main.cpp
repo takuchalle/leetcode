db2f2f100079cb1d1759168efb7a9c87c9d2a396
takuchalle <me@takuchalle.dev>
2022-06-22 18:10:36 +0900

    Update 215

--- 200/215/main.cpp
+++ 200/215/main.cpp
@@ -1,16 +1,41 @@
+#include <cstdlib>
 #include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 #define rep(i,n) for(int i = 0; i < (n); ++i)
 
 class Solution {
 public:
+#if 0
+	// sort all array approach
     int findKthLargest(vector<int>& nums, int k) {
 		sort(nums.begin(), nums.end(), greater<int>());
 		return nums[k-1];
+    }
+#endif
+	// priority queue approach
+    int findKthLargest(vector<int>& nums, int k) {
+		priority_queue<int, vector<int>, greater<int>> pq;
+
+		size_t i = 0;
+		for (i = 0; i < k; i++) {
+			pq.push(nums[i]);
+			cout << pq.top() << endl;
+		}
+
+
+		for (; i < nums.size(); i++) {
+			if(pq.top() < nums[i]) {
+				pq.pop();
+				pq.push(nums[i]);
+			}
+		}
+
+		return pq.top();
     }
 };
 
