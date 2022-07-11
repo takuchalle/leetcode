7feda216a5d630e5e4addfc3e2d11303c8c428a0
takuchalle <me@takuchalle.dev>
2022-07-11 09:51:59 +0900

    Update listnode

--- listnode.h
+++ listnode.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <vector>
+#include <iostream>
+
 struct ListNode {
     int val;
     ListNode *next;
@@ -6,3 +9,21 @@ struct ListNode {
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
+
+static ListNode* array2list(std::vector<int>& array) {
+	ListNode dummy = ListNode();
+	ListNode* node = &dummy;
+    for (auto var : array) {
+		node->next = new ListNode(var);
+		node = node->next;
+    }
+	return dummy.next;
+}
+
+static void showList(ListNode* node) {
+	for (ListNode* n = node; n != nullptr ; n = n->next) {
+		std::cout << n->val;
+		if(n->next) std::cout << " -> ";
+	}
+	std::cout << std::endl;
+}
