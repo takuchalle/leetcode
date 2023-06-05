51a422930dbe5ee50545e9d127408936644349ab
takuchalle <me@takuchalle.dev>
2023-06-05 11:00:11 +0900

    Update 1232

--- 1200/1232/main.cpp
+++ 1200/1232/main.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 
 using namespace std;
-
+#if 0
 class Solution {
    public:
     bool checkStraightLine(vector<vector<int>>& coordinates) {
@@ -32,6 +32,26 @@ class Solution {
         return true;
     }
 };
+#else
+class Solution {
+   public:
+    bool checkStraightLine(vector<vector<int>>& coordinates) {
+        const auto n = coordinates.size();
+
+        double y0 = (coordinates[1][1] - coordinates[0][1]);
+        double x0 = (coordinates[1][0] - coordinates[0][0]);
+
+        for (size_t i = 2; i < n; i++) {
+            double y = (coordinates[i - 1][1] - coordinates[i][1]);
+            double x = (coordinates[i - 1][0] - coordinates[i][0]);
+
+            if (y * x0 != y0 * x) return false;
+        }
+
+        return true;
+    }
+};
+#endif
 
 int main(int argc, char* argv[]) {
     auto s = new Solution();
